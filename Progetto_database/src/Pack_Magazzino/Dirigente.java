fbf48ecd3d96bde60d54844086bf5e7a5fab4829
AndreaSerafini <andreaserafinis@gmail.com>
2019-10-23 16:55:00 +0200

    5.2

--- Progetto_database/src/Pack_Magazzino/Dirigente.java
+++ Progetto_database/src/Pack_Magazzino/Dirigente.java
@@ -1,6 +1,9 @@
 package Pack_Magazzino;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import Pack_Magazzino.Persona;
 
@@ -8,6 +11,7 @@ public class Dirigente extends Persona{
 
 	private String data_nascita;
 	private String mail;
+	private Scanner scan  = new Scanner(System.in);
 
 	public Dirigente(String cf, String nome, String cognome) {
 		super(cf,nome,cognome);	
@@ -46,7 +50,101 @@ public class Dirigente extends Persona{
 
 	@Override
 	public void startOptions(Statement stmt) {
-		// TODO Auto-generated method stub
+		
+		int scelta;
+		String sql;
+		ResultSet rs = null;
+		
+		do {
+		System.out.println("Opzioni: ");
+		System.out.println("* AGGIUNGI PERSONALE:");
+		System.out.println("	1) Custode");
+		System.out.println("	2) Magazziniere");
+		System.out.println("	3) Impiegato");
+		System.out.println("	4) Fattorino");
+		System.out.println("	5) Veicolo");
+		System.out.println("* VISUALIZZA ORGANICO:");
+		System.out.println("	6) Custodi");
+		System.out.println("	7) Magazzinieri");
+		System.out.println("	8) Impiegati");
+		System.out.println("	9) Veicoli");
+		System.out.println("	10) Magazzini e relativi spazi");
+		System.out.println("	11) Tutti i clienti");
+		System.out.println("	12) Clienti della tua filiale");
+		System.out.println("0) Esci");
+		
+		System.out.print("\nScelta: ");
+		scan.nextLine();
+		scelta = scan.nextInt();
+		
+		switch(scelta)
+		{
+			case 1:
+				
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				sql = "select * from custode";
+				try {
+					rs = stmt.executeQuery(sql);
+					System.out.println("Codice fiscale | Nome | Cognome | Data di nascita | Telefono | Mail");
+					this.display(rs, 6);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 7:
+				sql = "select * from magazziniere";
+				try {
+					rs = stmt.executeQuery(sql);
+					System.out.println("Codice fiscale | Nome | Cognome | Data di nascita | Telefono | Mail");
+					this.display(rs, 6);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 8:
+				sql = "select * from impiegato";
+				try {
+					rs = stmt.executeQuery(sql);
+					System.out.println("Codice fiscale | Nome | Cognome | Data di nascita | Telefono | Mail");
+					this.display(rs, 6);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 9:
+				sql = "select * from veicolo";
+				try {
+					rs = stmt.executeQuery(sql);
+					System.out.println("Targa | Marca | Modello | Capacita'");
+					this.display(rs, 4);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 10:
+				break;
+			case 11:
+				break;
+			case 12:
+				break;
+			default:
+				break;
+		}
+		
+		}while(scelta != 0 && scelta < 13);
 		
 	}
 
